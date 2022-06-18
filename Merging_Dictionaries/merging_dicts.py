3a90bfdfaf623eb00ef5ccfc210091cc7e521f05
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2022-06-18 21:40:37 +0300

    Merging Dictionaries , chapter two)

---
+++ Merging_Dictionaries/merging_dicts.py
@@ -0,0 +1,9 @@
+name1 = {"kelly": 23, 
+"Derick": 14, "John": 7}
+name2 = {"Ravi": 45, "Mpho": 67}
+names = name1 | name2
+print(names)
+
+#Output:
+#{'kelly': 23, 'Derick': 14, 'John': 7, 'Ravi': 45, 
+#'Mpho': 67}
