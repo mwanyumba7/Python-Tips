8e7827654570ebbc5ccf3cdd8b863ec9a08a7433
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2022-06-19 22:33:01 +0300

    Calender module functions)

---
+++ Python_Calender/calender.py
@@ -0,0 +1,23 @@
+# Python has a built-in module calender that one can utilize to view the calendar using python
+
+# First we import the module
+import calendar
+
+# Lets print July 2022 using the month class and passing the year and month as an argument
+july = calendar.month(2022, 7)
+
+# Let's print the month of July
+print(july)
+
+#Output
+July 2022
+Mo Tu We Th Fr Sa Su
+             1  2  3
+ 4  5  6  7  8  9 10
+11 12 13 14 15 16 17
+18 19 20 21 22 23 24
+25 26 27 28 29 30 31
+
+# There are many other functions that can be performed by this module and are found here ( https://docs.python.org/3/library/calendar.html#calendar.HTMLCalendar.formatmonth )
+
+
