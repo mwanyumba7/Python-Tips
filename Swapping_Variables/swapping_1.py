533f412452e43dbfb3aec5e43bd08de3b605c274
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2022-06-22 23:24:36 +0300

    First method for Swapping variables

---
+++ Swapping_Variables/swapping_1.py
@@ -0,0 +1,13 @@
+# We first declare two variabes and assing them numbers
+x, y = 60, 90
+
+# We then swap the variables 
+x, y = y, x
+
+# We then print to see if it has worked
+print("x is", x)
+print("y is", y)
+
+# Output
+x is 90
+y is 60
