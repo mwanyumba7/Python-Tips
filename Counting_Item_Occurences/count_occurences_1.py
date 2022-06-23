0f3cb00525f998c23e48b3a3c8601ccbaae48395
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2022-06-23 23:21:00 +0300

    Number of times an item appears in an itereable

---
+++ Counting_Item_Occurences/count_occurences_1.py
@@ -0,0 +1,16 @@
+# We can be able to know how many times an item appers in an itereable. We cac use the counter() class
+
+# First we import counter
+from collections import Counter
+
+# We then declare a list that has some repeated values in it
+classmates = ["Consolata", "Gospel", "Marlin", "Consolata", "Marlin", "Fred"]
+
+# Lets now use the counter class to see how many times Consolata has been repeated
+count_conny = Counter(classmates).get("Consolata")
+
+# Lets print out and see how many times the name appeared
+print(f"The name Consolata appears in the list" f{count_consolata}items.")
+
+# Output The name 
+The name Consolata appears in the list 2
