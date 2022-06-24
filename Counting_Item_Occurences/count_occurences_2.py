afaaf527f3756447906aaba7e8aef66efe0a5be3
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2022-06-24 23:55:32 +0300

    Counting occurenc second way

---
+++ Counting_Item_Occurences/count_occurences_2.py
@@ -0,0 +1,9 @@
+fruits = ["Guava", "Apple", "Tangerine", "Apple"]
+
+count = 0
+for name in fruits:
+	if name == "Apple":
+		count +=
+
+print(f"The name Apple appears in the list"f"{count} times.")
+
