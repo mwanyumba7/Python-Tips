4c55bbe294527ba1539db26c17f9127cfb07aea6
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2022-08-26 23:01:45 +0300

    Add on how to find Key Words in Python

---
+++ Py_Reserved_Key_Words/reserved.py
@@ -0,0 +1,3 @@
+#Use the help() method to learn about the reserved keywords in Python. Keep in mind that none of these terms may be used as variable names. There will be a problem with your code.
+
+print(help('keywords'))
