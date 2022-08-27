7bd7bd3ae95edac2ba324433ed7d224d4df6bbdf
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2022-08-27 23:07:18 +0300

    Add Tip on Opening a Websir te using Python

---
+++ Open_Website_Using_Python/open_website.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+website = https://www.google.com/
+
+# This opens a new tab in your browser
+webbrowser.open_new_tab(website)
+# This opens a new browser window
+webbrowser.open_new(website)
