278d967213b86025244c0679c4895b4fa2437c61
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2022-09-06 22:12:09 +0300

    Printing Text With different color Using Python

---
+++ Printing_Colored_Text/colored_text.py
@@ -0,0 +1,11 @@
+class Colors():
+Black = '\033[30m'
+Green = '\033[32m'
+Blue = '\033[34m'
+Magenta = '\033[35m'
+Red = '\033[31m'
+Cyan = '\033[36m'
+White = '\033[37m'
+Yellow = '\033[33m'
+print(f'{Colors.Red} Warning: {Colors.Green} '
+f'Love Don\'t live here anymore')
